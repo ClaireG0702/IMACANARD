9b889bf3f139d5ed9d8a39c2055691fbe32d5df8
OceaneDrapeau <ocedrapeau@gmail.com>
2025-05-06 10:41:27 +0200

    add block struct

--- src/includes/character.hpp
+++ src/includes/character.hpp
@@ -5,3 +5,12 @@
 // ne pas passer à travers les blocs pleins
 // position de départ
 // action avec les blocs en bonus
+
+/*Gestion des collisions
+- je suis dans une tuile si au moins des 4 corners est lui aussi dans la grille
+- pour vérifier : prendre flottant --> arrondir au précédent (avec un round ou un float)
+
+je calcule position possible + est ce que dans les 4 tuiles possibles y en a 1 qui est pleine ?
+    - si oui : j'avance
+    - si non : je bouge pas ou action
+*/
--- src/includes/map.hpp
+++ src/includes/map.hpp
@@ -3,3 +3,26 @@
 // génération map
 // placement objets
 // placement aléatoire
+// gestion des blocs
+
+enum class BlockType
+{
+    EMPTY,    // water
+    PLAIN,    // bloc qu'on peut détruire --> nénuphar
+    TRAP,     // piège
+    OBSTACLE, // wood, rock
+    OBJECT,   // shadow + anim si on a le temps
+};
+
+// enum class Actions
+// {
+//     MINE,
+//     SPEED,
+//     SLOW,
+// }
+
+struct Block
+{
+    BlockType type{};
+    // bool object{};
+};
