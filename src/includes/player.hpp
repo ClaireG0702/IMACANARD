ff7127ff8e6a728dc7d54c08fe867ae13c8e0f94
OceaneDrapeau <ocedrapeau@gmail.com>
2025-06-05 23:43:55 +0200

    clean player

--- src/includes/player.hpp
+++ src/includes/player.hpp
@@ -1,19 +1,15 @@
 #pragma once
 #include "includes/map.hpp"
 
-// mouvement
+struct Player
+{
+    glm::vec2 position{};
+    glm::vec2 gridPos{};
+    Direction direction{};
+    float speed{2.0f};
+    int objectsNbr{0};
 
-// manger les ombres
-// minage (détruire un bloc)
-
-struct Player {
-    glm::vec2 position {};
-    glm::vec2 gridPos {};
-    Direction direction {};
-    float speed {2.0f};
-    int objectsNbr {0};
-
-    void digging (Cell& block);
+    void digging(Cell &block);
 };
 
 void initPlayer(Player &player, std::vector<Cell> &map);
